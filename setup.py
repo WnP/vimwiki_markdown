d3e72294a2b4a1dd96e43793a41f35b62175b1da
Steeve Chailloux <steeve.chailloux@proton.ch>
2023-10-16 10:35:33 +0200

    release 0.4.1
    Signed-off-by: Steeve Chailloux <steeve.chailloux@proton.ch>

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ setup(
     "vimwiki markdown file to html with syntax highlighting.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.4.0",
+    version="0.4.1",
     py_modules=["vimwiki_markdown"],
     packages=[],
     package_data={},
     license="MIT License",
     author="Steeve Chailloux",
-    author_email="steeve" "@" "chailloux.me",
+    author_email="github.unrented556" "@" "passmail.net",
     url="https://github.com/WnP/vimwiki_markdown/",
     entry_points={
         "console_scripts": [["vimwiki_markdown = vimwiki_markdown:main"]]
